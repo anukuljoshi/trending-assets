622210c2c96b2281bbcd9194bb0903f790129060
Anukul Joshi <anukuljoshi1@gmail.com>
2022-08-23 13:54:21 +0530

    added tailwind to project and svgs for icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 
+import { ReactComponent as Ethereum } from "./assets/svg/Ethereum.svg";
+import { ReactComponent as BinanceCoin } from "./assets/svg/BinanceCoin.svg";
+import { ReactComponent as Bitcoin } from "./assets/svg/BitcoinSV.svg";
+import { ReactComponent as Shiba } from "./assets/svg/SHIBA.svg";
+import { ReactComponent as Solana } from "./assets/svg/Solana.svg";
+
 function App() {
 	return (
 		<div>
-			<h1>Trending Assets</h1>
+			<h1 className="text-3xl text-red-800">Trending Assets</h1>
+			<div>
+				<Ethereum />
+				<BinanceCoin />
+				<Bitcoin />
+				<Shiba />
+				<Solana />
+			</div>
 		</div>
 	);
 }
